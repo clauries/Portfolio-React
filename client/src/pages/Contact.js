4ed9fc150bff1fef601be7f39e03c3f729a342f3
clauries <55369403+clauries@users.noreply.github.com>
2020-05-07 16:03:35 -0700

    Updated contact info & removed form as it is not currently working

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -54,14 +54,15 @@ function Contact() {
                         <Grid container direction="column" justifyContent="center" alignItems="center">
                             <CardContent>
                                 <form className={classes.root} noValidation method="post" enctype="text/plain" data-aos="fade-up" data-aos-delay="300">
-                                    <p id="instructions">Leave me a quick note below or email me directly: laurie@laurieschroeder.net</p>
+                                <p id="instructions">Please contact me via LinkedIn or email me directly: laurjschroeder@gmail.com</p>
+                                    {/* <p id="instructions">Leave me a quick note below or email me directly: laurie@laurieschroeder.net</p>
                                     <br />
                                     <TextField id="name" label="What is your name?" color="primary" variant="outlined" fullWidth required />
                                     <TextField id="email" label="What is your e-mail?" color="primary" variant="outlined" fullWidth required />
                                     <TextField id="message" label="Message" multiline rows="6" variant="outlined" fullWidth required />
                                     <Grid item alignItems="flex-start">
                                         <Button variant="contained" color="primary">Send Message</Button>
-                                    </Grid>
+                                    </Grid> */}
                                 </form>
                             </CardContent>
                         </Grid>
